fc013e4c0ba115de7d21f3b2a571f2234a36f1ad
Billy <hchoi92@hotmail.com>
2018-12-17 20:14:40 -0800

    Added data annotations

--- GigHub/GigHub/Models/Genre.cs
+++ GigHub/GigHub/Models/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace GigHub.Models
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
         public string Name { get; set; }
     }
 }
--- GigHub/GigHub/Models/Gig.cs
+++ GigHub/GigHub/Models/Gig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,23 @@ namespace GigHub.Models
     public class Gig
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
         public ApplicationUser Artist { get; set; }
+
+        [Required]
         public DateTime DateTime { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Venue { get; set; }
+
+        [Required]
+        [MaxLength(128)]
         public string Name { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }        
     }
 }
