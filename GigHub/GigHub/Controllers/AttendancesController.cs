562305d28cf4a4bf980706ab348aab6485846a38
Billy Choi <bchoi@pnimedia.com>
2019-01-16 19:31:42 -0800

    Web Api

---
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();            
+            return Ok();
+        }        
+    }
+}
