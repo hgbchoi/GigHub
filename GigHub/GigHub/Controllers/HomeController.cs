8df14304b29d15063adcef9f63af9c04f26d7f6f
Billy Choi <bchoi@pnimedia.com>
2019-01-17 19:32:39 -0800

    Changed css and hide buttons from unauthorized users

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
 ﻿using GigHub.Models;
 using System;
 using System.Data.Entity;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GigHub.ViewModels;
 
 namespace GigHub.Controllers
 {
@@ -24,7 +24,13 @@ namespace GigHub.Controllers
                 .Include(g => g.Artist)
                 .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
-            return View(upcomingGigs);
+
+            var viewModel = new HomeViewModel
+            {
+                UpcomingGigs = upcomingGigs,
+                ShowActions = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
 
         public ActionResult About()
