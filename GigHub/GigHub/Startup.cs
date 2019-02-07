cc09f93150ee6c65fca4adf9c5494023b6bcc46f
Billy Choi <bchoi@pnimedia.com>
2019-02-07 13:23:43 -0800

    Remove Release.config from tracking

--- GigHub/GigHub/Startup.cs
+++ GigHub/GigHub/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using System.Configuration;
+using System.Web.Configuration;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(GigHub.Startup))]
@@ -7,7 +9,7 @@ namespace GigHub
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
-        {
+        {           
             ConfigureAuth(app);
         }
     }
